9ee009e76ea7199908147dd343e2dd0a3d2c653c
JOSE ROBERTO BERNARDES JUNIOR <012100199@hsm.int>
2023-11-08 09:53:14 -0300

    Sistema estavel

--- ProntuarioDigitalOdonto/dist/assets/js/atenderPaciente.js
+++ ProntuarioDigitalOdonto/dist/assets/js/atenderPaciente.js
@@ -4,13 +4,17 @@ const numerosEmIngles = ['',
     'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'
 ];
 let quantidadeAtestados = 1
+let quantidadePrescricao = 1
 
 //ENUM
 const itemSelecionado={
     evolucao : 0,
     prescricao : 1,
-    atestado : 2
+    atestado : 2,
+    btnAddAtestado : 3,
+    btnAddPrescricao : 4
 }
+
 
 // ELEMENTOS DA PAGINA INDEX
 const iconeEvolucao = document.getElementById("iconeEvolucao") 
@@ -29,8 +33,13 @@ const accordionAtestado = document.getElementById("accordionAtestado")
 const inputAtesdadoCid = document.getElementById("inputAtesdadoCid")
 const inputAtesdadoQtd = document.getElementById("inputAtesdadoQtd")
 const inputAtesdadoComplemento = document.getElementById("inputAtesdadoComplemento")
-
 const btnAddAtestado = document.getElementById("btnAddAtestado")
+
+const accordionReceituario = document.getElementById("accordionReceituario")
+const inputPrescricao = document.getElementById("inputPrescricao")
+const inputPrescricaoQtd = document.getElementById("inputPrescricaoQtd")
+const inputPrescricaoRecomendacoes = document.getElementById("inputPrescricaoRecomendacoes")
+const btnAddPrescricao = document.getElementById("btnAddPrescricao")
 
 //FUNCOES DA PAGINA INDEX
 
@@ -73,7 +82,7 @@ const gerarAtestado =()=>{
 
     componente +="<button class=\"accordion-button collapsed\" type=\"button\"data-bs-toggle=\"collapse\" data-bs-target=\"#collapse"+ numerosEmIngles[quantidadeAtestados] +"\" aria-expanded=\"false\" aria-controls=\"collapse"+ numerosEmIngles[quantidadeAtestados] +"\">"
 
-    componente +="Atestado"+" "+dataAtualPadraoPagina()+"<span onclick=\"removerAtestado(Atestado"+ quantidadeAtestados +")\" class=\"apagar\">&emsp;🗑</span>"
+    componente +="Atestado"+" "+dataAtualPadraoPagina()+"<span onclick=\"removerItem(Atestado"+ quantidadeAtestados +","+ itemSelecionado.btnAddAtestado +")\" class=\"apagar\">&emsp;🗑</span>"
 
     componente +="</button>"
     componente +="</h4>"
@@ -81,7 +90,7 @@ const gerarAtestado =()=>{
 
     componente +="<div class=\"accordion-body\">"
     componente +="<p>"
-    componente += "CID:" + inputAtesdadoCid.value.toUpperCase() +"<br/>"+ inputAtesdadoQtd.value+" dias de afastamento<br/>"+inputAtesdadoComplemento.value+"<br/><a href=\"\">Imprimir</a>"
+    componente += "CID:" + inputAtesdadoCid.value.toUpperCase() +"<br/>"+ inputAtesdadoQtd.value+" dias de afastamento<br/>"+inputAtesdadoComplemento.value+"<br/><span onclick=\"gerarPdf()\">Imprimir</span>"
     componente +="</p>"
     componente +="</div></div></div>"
 
@@ -89,10 +98,48 @@ const gerarAtestado =()=>{
     quantidadeAtestados += 1
 }
 
-const removerAtestado = (idAtestado) =>{
-    accordionAtestado.removeChild(idAtestado)
+const gerarPrescricao =()=>{
+    
+    let componente ="<div id=\"Prescricao"+ quantidadePrescricao +"\" class=\"accordion-item\">"
+    componente +="<h4 class=\"accordion-header\" id=\"heading"+ numerosEmIngles[quantidadePrescricao] +"\">"
+
+    componente +="<button class=\"accordion-button collapsed\" type=\"button\"data-bs-toggle=\"collapse\" data-bs-target=\"#collapse"+ numerosEmIngles[quantidadePrescricao] +"\" aria-expanded=\"false\" aria-controls=\"collapse"+ numerosEmIngles[quantidadePrescricao] +"\">"
+
+    componente += inputPrescricao.value+" "+inputPrescricaoQtd.value+"<span onclick=\"removerItem(Prescricao"+ quantidadePrescricao +","+ itemSelecionado.btnAddPrescricao +")\" class=\"apagar\">&emsp;🗑</span>"
+
+    componente +="</button>"
+    componente +="</h4>"
+    componente +="<div id=\"collapse"+ numerosEmIngles[quantidadePrescricao] +"\" class=\"accordion-collapse collapse\" aria-labelledby=\"heading"+ numerosEmIngles[quantidadePrescricao] +"\" data-bs-parent=\"#accordionExample\">"
+
+    componente +="<div class=\"accordion-body\">"
+    componente +="<p>"
+    componente += inputPrescricaoRecomendacoes.value+"<br/>"
+    componente +="</p>"
+    componente +="</div></div></div>"
+
+    accordionReceituario.innerHTML += componente
+    quantidadePrescricao += 1
+}
+
+const removerItem = (idItem, btn) =>{
+   if(btn === itemSelecionado.btnAddAtestado){
+      accordionAtestado.removeChild(idItem)
+   }
+
+   if(btn === itemSelecionado.btnAddPrescricao){
+      accordionReceituario.removeChild(idItem)
+   }    
+}
+
+const gerarPdf =()=>{
+    const doc = new jsPDF();
+
+    doc.text('O belisco não gosta de trabalhar', 10, 10);
+
+    doc.save('documento.pdf');
 }
 
 //EVENTOS DOM
 
-btnAddAtestado.addEventListener('click',gerarAtestado)
+btnAddAtestado.addEventListener('click' ,gerarAtestado)
+btnAddPrescricao.addEventListener('click', gerarPrescricao)
